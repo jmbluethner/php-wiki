9c076218860809af8a82fb5818d896d204ecea8a
mail@jan-morris.de <mail@jan-morris.de>
2019-07-30 20:51:59 +0200

    bla

---
+++ elements/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="three columns">
+                <h6>Kontakt</h6>
+            </div>
+            <div class="three columns">
+                <h6>Rechtliches</h6>
+            </div>
+            <div class="three columns">
+                <h6>Über mich</h6>
+            </div>
+            <div class="three columns">
+                <h6></h6>
+            </div>
+        </div>
+    </div>
+</footer>
